a4722f6bde874b7b5d458373d83548d3c3d828b5
ahmere-s <csmail.santos@gmail.com>
2026-04-25 02:40:34 -0400

    starting loop

--- smartHome/main/main.ino
+++ smartHome/main/main.ino
@@ -23,8 +23,12 @@ void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(1, 0);
   lcd.print("Welcome, Guest!");
+  lcd.setCursor(2, 1);      //setCursor() takes (column, row) NOT vice versa.
+  lcd.print("System Ready.");
+  delay(4000);
+  lcd.clear();
 
   pinMode(sensorMotion, INPUT);
   pinMode(buttonSwitch1, INPUT_PULLUP);
@@ -40,5 +44,31 @@ void setup() {
 }
 
 void loop(){
+  //Get/read current state of all sensors
+  int gasValue = analogRead(sensorGas);
+  int pin4Butt = digitalRead(buttonSwitch1);
+  int pin5Butt = digitalRead(buttonSwitch2);
+  int motionSense = digitalRead(sensorMotion);
+
+  //Here I will begin to order sensor logic based on priority and security purposes
+  lcd.setCursor(0, 0);    // Dont have to clear the first row since printing something new will replace old characters. 
+  lcd.print("Gas Level: ");
+  lcd.setCursor(11, 0);
+  lcd.print(gasValue);
+  delay(1000);
+
+  if (gasValue > 400){
+    
+  }
+  else {
+    
+  }
+
+  //if (motionSense == HIGH){} else {}
+  
+
+
+
+
 
 }
